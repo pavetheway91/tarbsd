cd81dbdfbc08eaff67924b5060af613e6758807e
Paavo-Einari Kaipila <pave@freebsd.org>
2025-08-12 02:06:32 +0300

    don't touch error reporting settings on a ports edition

--- src/Util/PlatformCheck.php
+++ src/Util/PlatformCheck.php
@@ -5,10 +5,13 @@ class PlatformCheck
 {
     public static function run() : void
     {
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
-        set_time_limit(0);
+        if (!TARBSD_PORTS)
+        {
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+            set_time_limit(0);
+        }
 
         /**
          * Phar archive starts with similiar checks
